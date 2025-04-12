4b102f45ee702ff7968ea384352f3b0ff9ca3a1a
choguun <visaruth.s@gmail.com>
2025-04-12 13:33:02 +0900

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ import os
 dotenv.load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 LLM_MODEL = os.getenv("LLM_MODEL")
 WALLET_PRIVATE_KEY=os.getenv("WALLET_PRIVATE_KEY")
 RPC_PROVIDER_URL=os.getenv("RPC_PROVIDER_URL")
